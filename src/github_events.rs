0cb747aef2f9776cafc1941eefa3471e0d363644
Łukasz Huculak <ukasz.apps@gmail.com>
2023-03-28 20:32:44 +0200

    Update git webhook event model

--- src/github_events.rs
+++ src/github_events.rs
@@ -7,6 +7,7 @@ pub struct PushEvent {
     pub branch_ref: String,
     pub head_commit: Commit,
     pub commits: Vec<Commit>,
+    pub repository: Repository,
 }   
 
 
@@ -25,4 +26,23 @@ pub struct Commit {
 
     pub message: String,
 
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Repository {
+    pub id: u32,
+
+    pub node_id: String,
+
+    pub name: String,
+
+    pub full_name: String,
+
+    pub private: bool,
+
+    pub html_url: String,
+
+    // API urls
+    //TODO: deserialize the pattern into specialized trait
+    pub contents_url: String,
 }
